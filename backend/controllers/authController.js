5d3ded138ff0e26822258cf2def02ff9dc57c51a
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-02 09:38:17 +1000

    TASK-6 Subtask 1.1: Create the signup form component.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,6 +7,7 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
+// Register User Function:
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     try {
